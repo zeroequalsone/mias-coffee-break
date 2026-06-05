7e49fc56dcae99dde8cd74de27c2969925af30da
zeroequalsone <real.rizzle74@gmail.com>
2026-06-05 23:57:42 +0200

    Made footer responsive

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -12,28 +12,41 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center bg-brown-dark text-orange gap-8 py-12">
       <div className="text-sm flex flex-col items-center gap-8">
-        <p className="font-knicknack text-4xl">
+        <p className="font-knicknack text-3xl lg:text-4xl">
           Mia&apos;s&nbsp;Coffee&nbsp;Break
         </p>
-        <ul className="flex items-center gap-8">
+        <ul className="flex flex-col lg:flex-row items-center gap-2 lg:gap-8">
           {footerLinks.map((footerLink, index) => (
             <Fragment key={footerLink.href}>
               <li className="text-lg">
                 <Link href={footerLink.href}>{footerLink.name}</Link>
               </li>
               {index < footerLinks.length - 1 && (
-                <span className="text-2xl">•</span>
+                <span className="text-2xl hidden lg:block">•</span>
               )}
             </Fragment>
           ))}
         </ul>
       </div>
       <span className="border-t w-full"></span>
-      <div className="text-sm">
+      <div className="text-sm lg:text-base text-center space-y-2">
+        <div>
+          <p>
+            &copy;&nbsp;{new Date().getFullYear()}
+            &nbsp;Mia&apos;s&nbsp;Coffee&nbsp;Break.
+          </p>
+          <p>Alle&nbsp;Rechte&nbsp;vorbehalten.</p>
+        </div>
         <p>
-          &copy;&nbsp;{new Date().getFullYear()}
-          &nbsp;Mia&apos;s&nbsp;Coffee&nbsp;Break.
-          Alle&nbsp;Rechte&nbsp;vorbehalten.
+          Portfolio Projekt von{" "}
+          <Link
+            href={"https://github.com/zeroequalsone"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-cream active:text-cream"
+          >
+            zeroequalsone
+          </Link>
         </p>
       </div>
     </footer>
