44f708a6655b30e9c09270c2d09c4cc7a9d868ff
zeroequalsone <real.rizzle74@gmail.com>
2026-06-05 22:30:04 +0200

    Made coffee section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -21,20 +21,22 @@ export default function Home() {
       </section>
 
       {/* Coffee Section */}
-      <section className="flex items-center justify-between gap-8 p-16 bg-brown-dark">
-        <div className="flex flex-col gap-6 text-orange">
-          <p className="font-knicknack text-7xl">
+      <section className="flex flex-col lg:flex-row items-center justify-between gap-8 mt-12 lg:mt-24 py-8 lg:px-16 lg:p-16 bg-brown-dark">
+        <div className="flex flex-col gap-4 lg:gap-6 text-orange">
+          <p className="font-knicknack text-4xl lg:text-7xl text-center lg:text-left">
             <span className="block">Good Coffee.</span>
             <span className="block">Good Vibes.</span>
           </p>
-          <p className="font-pacifico text-3xl">Schmeckt wie in Italien</p>
         </div>
         <Image
           alt="Coffee Cup"
           src={CoffeeCup}
-          className="w-96 -rotate-10"
+          className="w-48 lg:w-96 -rotate-10"
           loading="eager"
         />
+        <p className="font-pacifico italic text-xl lg:text-3xl text-orange">
+          Schmeckt wie in Italien
+        </p>
       </section>
     </Fragment>
   );
