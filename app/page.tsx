e54b137f98f07e3185827f9aa58c3a3cce1c598b
zero <98187128+zeroequalsone@users.noreply.github.com>
2026-06-06 23:09:21 +0200

    Feature: Added coffee banner (#13)

--- app/page.tsx
+++ app/page.tsx
@@ -65,6 +65,24 @@ export default function Home() {
           </div>
         ))}
       </section>
+
+      {/* Coffee Banner */}
+      <section>
+        <div className="relative h-48 lg:h-96 overflow-hidden">
+          <Image
+            src={"/CoffeeBeansBanner.webp"}
+            alt="Kaffeebohnen Banner"
+            className="object-cover scale-150 lg:scale-none"
+            loading="eager"
+            fill
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <p className="w-48 lg:w-96 text-center font-sundayNotes text-lg lg:text-4xl text-brown-dark">
+              Auch Kaffeebohnen brauchen einen Break. Nimm dir deinen.
+            </p>
+          </div>
+        </div>
+      </section>
     </Fragment>
   );
 }
