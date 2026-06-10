ccdbc4a79b70fe3077a8697dd587ce90cdb8730a
zeroequalsone <real.rizzle74@gmail.com>
2026-06-10 23:51:26 +0200

    Added Ende Section

--- app/page.tsx
+++ app/page.tsx
@@ -81,6 +81,19 @@ export default function Home() {
           </p>
         </div>
       </section>
+
+      {/* Ende Section */}
+      <section className="h-48 lg:h-96 my-16">
+        <div className="flex flex-col items-center justify-center h-full text-orange text-xl lg:text-3xl font-knicknack">
+          <div className="flex flex-col items-center justify-center w-96 lg:w-3xl text-center gap-8">
+            <p className="text-3xl lg:text-6xl">
+              Du hast es bis zum Ende der Seite geschafft!
+            </p>
+            <p>Jetzt fehlt nur noch dein perfekter Kaffee.</p>
+            <p>See you at the coffee break? 👀</p>
+          </div>
+        </div>
+      </section>
     </Fragment>
   );
 }
