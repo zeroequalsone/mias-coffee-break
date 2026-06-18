7c0676eb83529ec3d56886af51ec8bffaf391f79
zeroequalsone <real.rizzle74@gmail.com>
2026-06-18 16:30:06 +0200

    feature: added story section

--- app/(main)/meet-mias/page.tsx
+++ app/(main)/meet-mias/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/sections/meet-mias/Hero";
+import { Story } from "@/components/sections/meet-mias/Story";
 
 export default function MeetMias() {
   return (
     <>
       <Hero />
+      <Story />
     </>
   );
 }
---
+++ app/components/sections/meet-mias/Story.tsx
@@ -0,0 +1,19 @@
+export const Story = () => {
+  return (
+    <div className="mt-6 bg-brown-dark text-center text-orange-dark">
+      <div className="flex flex-col max-w-4xl mx-auto p-8 lg:p-12 gap-6">
+        <p className="font-knicknack text-3xl lg:text-6xl">Unsere Geschichte</p>
+        <p className="lg:text-xl">
+          Mia's Coffee Break entstand aus der Leidenschaft für authentischen
+          Kaffee, köstliche Focaccias, leckere Snacks und erfrischenden Matcha.
+          Unsere Vision ist es, einen Ort zu schaffen, an dem Menschen eine
+          echte Pause vom Alltag machen können.
+        </p>
+        <p className="text-sm italic max-w-sm mx-auto">
+          Inspiriert von italienischen Kaffeehäusern, wo jeder Schluck und jeder
+          Bissen eine Geschichte erzählt.
+        </p>
+      </div>
+    </div>
+  );
+};
